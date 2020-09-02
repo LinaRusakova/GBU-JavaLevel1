32d6dd1672f1f700bc0e1cc2b5c47c733f2fa703
Lina Rusakova <xlinaris@gmail.com>
2020-09-02 14:53:51 +0300

    My homework for lesson4. Tasks 1,2,3,4 completed. In addition, the computer checks the free possible winning moves before making a further move.

--- Homework/src/lesson04/lesson04.java
+++ Homework/src/lesson04/lesson04.java
@@ -8,6 +8,7 @@ package lesson04;
  */
 
 
+import java.util.Random;
 import java.util.Scanner;
 
 
@@ -27,6 +28,8 @@ public class lesson04 {
     static String userAct; // строковая переменная, введеного игроком числа
     public static boolean winnerGamer; // переменная, для определения победиителя в игре
 
+    static int winnXCoordinate;
+    static int winnYCoordinate;
 
     public static void main(String[] args) {
         //Сначала вызовем метод для создания игровой карты.
@@ -95,49 +98,67 @@ public class lesson04 {
             } else { // если всё в порядке, то
                 System.out.println(tempValue); // выводим игровое поле с ходом игрока
                 tempValue = "";// сбрасываем значение переменной, выводящей игровое поле в консоль
-                if (checkWinner(x)) {
+                if (checkWinner(x, false)) {
                     System.out.println("Победил человек! Игра окончена.");
                     isNext = false;
                 }
             }
         }
     }
+
     // описываем логику ходов компьютера
     private static void playGameO() {
         if (isNext && isContinue()) {
-            int random = (int) (Math.random() * mapSize * mapSize);
-            String AiAct = String.valueOf(random);
-            int i, j;
+            String AiAct = "";
             isRepeat = true;
-            for (i = 0; i < mapSize; i++) {
-                for (j = 0; j < mapSize; j++) {
-
-
-                    if (AiAct.equals(gameMap[i][j]) && !AiAct.equals(x) && !AiAct.equals(o)) {
-
-                        gameMap[i][j] = o;
-                        isRepeat = false;
+            while (isRepeat) {
+            if (checkWinner(o, true)) { //проверяем, может ли на этом ходе победить компьютер
+                if (!x.equals(gameMap[winnXCoordinate][winnYCoordinate]) && !o.equals(gameMap[winnXCoordinate][winnYCoordinate])) { //проверяем не стоит ли уже что-то в этом месте.
+                    AiAct=gameMap[winnXCoordinate][winnYCoordinate];
+                    gameMap[winnXCoordinate][winnYCoordinate] = o;
+                }
+            }
+                if (checkWinner(x, true)) { //если нет, то проверяем, может ли на этом ходе победить человек, если да, то блокируем его
+                    if (!x.equals(gameMap[winnXCoordinate][winnYCoordinate]) && !o.equals(gameMap[winnXCoordinate][winnYCoordinate])) {
+                        AiAct = gameMap[winnXCoordinate][winnYCoordinate];
+                        gameMap[winnXCoordinate][winnYCoordinate] = o;
                     }
+                } else { //если не то и не другое, то запускаем рандомный шаг компьютера
+
+                    Random random = new Random();
+                    int numberAi = random.nextInt(mapSize * mapSize);
+                    AiAct = String.valueOf(numberAi);
+                    for (int i = 0; i < mapSize; i++) {
+                        for (int j = 0; j < mapSize; j++) {
+                            if (AiAct.equals(gameMap[i][j]) && !AiAct.equals(x) && !AiAct.equals(o)) {
+                                gameMap[i][j] = o;
+                                isRepeat = false;
+                                break;
+                            }
+                        }
+                    }
+
+                }
+            }
+
+            //формируем итоговое игровое поле после хода компьютера
+            for (int i = 0; i < mapSize; i++) {
+                for (int j = 0; j < mapSize; j++) {
                     tempValue += gameMap[i][j] + "\t";
                 }
                 tempValue += "\n";
             }
-
-            if (isRepeat) { //если число некорректное
-                tempValue = "";// сбрасываем значение переменной, выводящей игровое поле в консоль
-                playGameO(); //рекурсивно запустим метод  playGameO();
-            } else { // если всё в порядке, то...
-                System.out.println("Копьютер ходит: " + AiAct + ".\n");
-                System.out.println(tempValue); // выводим игровое поле с ходом игрока
-                tempValue = "";// сбрасываем значение переменной, выводящей игровое поле в консоль
-                if (checkWinner(o)) {
-                    System.out.println("Победил компьютер! Игра окончена.");
-                    isNext=false;
-                }
-
+            System.out.println("Копьютер ходит: " + AiAct + ".\n");
+            System.out.println(tempValue); // выводим игровое поле с ходом игрока
+            tempValue = "";// сбрасываем значение переменной, выводящей игровое поле в консоль
+            if (checkWinner(o, false)) {
+                System.out.println("Победил компьютер! Игра окончена.");
+                isNext = false;
             }
+
         }
     }
+
 
     //Проверим, возможны ли ходы дальше, и не окончена ли уже игра.
     private static boolean isContinue() {
@@ -164,9 +185,10 @@ public class lesson04 {
     }
 
     //проверка ходов на победу
-    private static boolean checkWinner(String nameWinner) {
+    private static boolean checkWinner(String nameWinner, boolean simulation) {
+
         //проверяем победу на всех строках и столбцах, соответствующих условию количества выигрышных фишек и размеру игрового
-        winnerGamer=false;
+        winnerGamer = false;
         int row = 0, col = 0;
         for (int i = 0; i < mapSize; i++) {
             for (int j = 0; j < mapSize; j++) {
@@ -176,14 +198,34 @@ public class lesson04 {
                         winnerGamer = true;
                         break;
                     }
-                } else {
-                    row = 0;
-                }
+                    if (row == lengthWinner - 1 && simulation && j + 1 < mapSize) {
+                        if (o != gameMap[i][j + 1]) {
+                            winnXCoordinate = i;
+                            winnYCoordinate = j + 1;
+                            winnerGamer = true;
+                            break;
+                        }
+                    }
+                }else {
+                        row = 0;
+                    }
+            }
+        }
+        for (int i = 0; i < mapSize; i++) {
+            for (int j = 0; j < mapSize; j++) {
                 if (gameMap[j][i].equals(nameWinner)) {
                     col++;
                     if (col == lengthWinner) {
                         winnerGamer = true;
                         break;
+                    }
+                    if (col == lengthWinner - 1 && simulation && j + 1 < mapSize) {
+                        if (o != gameMap[j+1][i]) {
+                            winnXCoordinate = j+1;
+                            winnYCoordinate = i;
+                            winnerGamer = true;
+                            break;
+                        }
                     }
                 } else {
                     col = 0;
@@ -191,54 +233,86 @@ public class lesson04 {
             }
         }
 
-        //проверяем победу на всех диагоналях, соответствующих условию количества выигрышных фишек и размеру игрового поля
-        int d = 0;
-        while (d <= mapSize - lengthWinner) {
-            //проверяем победу на всех главных диагоналях, соответствующих условию количества выигрышных фишек и размеру игрового поля
-            for (int j = 0, diagonalH = 0, diagonalD = 0; j < mapSize - d; j++) {   //diagonalH- все диагонали выше главной, diagonalD все диагонали выше главной
-                if (gameMap[j + d][j].equals(nameWinner)) {
-                    diagonalH++;
-                    if (diagonalH == lengthWinner) {
-                        winnerGamer = true;
-                        break;
+            //проверяем победу на всех диагоналях, соответствующих условию количества выигрышных фишек и размеру игрового поля
+            int d = 0;
+            while (d <= mapSize - lengthWinner) {
+                //проверяем победу на всех главных диагоналях, соответствующих условию количества выигрышных фишек и размеру игрового поля
+                for (int j = 0, diagonalH = 0, diagonalD = 0; j < mapSize - d; j++) {   //diagonalH- все диагонали выше главной, diagonalD все диагонали выше главной
+                    if (gameMap[j + d][j].equals(nameWinner)) {
+                        diagonalH++;
+                        if (diagonalH == lengthWinner) {
+                            winnerGamer = true;
+                            break;
+                        }
+                        if (diagonalH == lengthWinner - 1 && simulation && j + d + 1 < mapSize && j + 1 < mapSize) {
+                            if (o != gameMap[j + d + 1][j + 1]) {
+                                winnXCoordinate = j + d + 1;
+                                winnYCoordinate = j + 1;
+                                winnerGamer = true;
+                                break;
+                            }
+                        }
+                    } else {
+                        diagonalH = 0;
                     }
-                } else {
-                    diagonalH = 0;
-                }
-                if (gameMap[j][j + d].equals(nameWinner)) {
-                    diagonalD++;
-                    if (diagonalD == lengthWinner) {
-                        winnerGamer = true;
-                        break;
+                    if (gameMap[j][j + d].equals(nameWinner)) {
+                        diagonalD++;
+                        if (diagonalD == lengthWinner) {
+                            winnerGamer = true;
+                            break;
+                        }
+                        if (diagonalD == lengthWinner - 1 && simulation && j + d + 1 < mapSize && j + 1 < mapSize) {
+                            if (!gameMap[j + 1][j + d + 1].equals(o)) {
+                                winnYCoordinate = j + d + 1;
+                                winnXCoordinate = j + 1;
+                                winnerGamer = true;
+                                break;
+                            }
+                        }
+                    } else {
+                        diagonalD = 0;
                     }
-                } else {
-                    diagonalD = 0;
                 }
+                //проверяем победу на всех второстепенных диагоналях, соответствующих условию количества выигрышных фишек и размеру игрового поля
+                for (int j = 0, diagonalH = 0, diagonalD = 0; j < mapSize - d; j++) {   //diagonalH- все диагонали выше главной, diagonalD все диагонали выше главной
+                    if (gameMap[j + d][mapSize - 1 - j].equals(nameWinner)) {
+                        diagonalH++;
+                        if (diagonalH == lengthWinner) {
+                            winnerGamer = true;
+                            break;
+                        }
+                        if (diagonalH == lengthWinner - 1 && simulation) {
+                            if (j + d + 1 < mapSize - d && mapSize - 1 - (j + 1) < mapSize - d && !gameMap[j + d + 1][mapSize - 1 - (j + 1)].equals(o)) {
+                                winnXCoordinate = j + d + 1;
+                                winnYCoordinate = mapSize - 1 - (j + 1);
+                                winnerGamer = true;
+                                break;
+                            }
+                        }
+                    } else {
+                        diagonalH = 0;
+                    }
+                    if (gameMap[j][mapSize - 1 - (j + d)].equals(nameWinner)) {
+                        diagonalD++;
+                        if (diagonalD == lengthWinner) {
+                            winnerGamer = true;
+                            break;
+                        }
+                        if (diagonalD == lengthWinner - 1 && simulation) {
+                            if (j + 1 < mapSize - d && mapSize - 1 - j + 1 < mapSize - d && !gameMap[j + 1][mapSize - 1 - (j + 1 + d)].equals(o)) {
+                                winnXCoordinate = j + 1;
+                                winnYCoordinate = mapSize - 1 - (j + 1 + d);
+                                winnerGamer = true;
+                                break;
+                            }
+                        }
+                    } else {
+                        diagonalD = 0;
+                    }
+                }
+                d++;
             }
-            //проверяем победу на всех второстепенных диагоналях, соответствующих условию количества выигрышных фишек и размеру игрового поля
-            for (int j = 0, diagonalH = 0, diagonalD = 0; j < mapSize - d; j++) {   //diagonalH- все диагонали выше главной, diagonalD все диагонали выше главной
-                if (gameMap[j + d][mapSize - 1 - j].equals(nameWinner)) {
-                    diagonalH++;
-                    if (diagonalH == lengthWinner) {
-                        winnerGamer = true;
-                        break;
-                    }
-                } else {
-                    diagonalH = 0;
-                }
-                if (gameMap[j][mapSize - 1 - (j + d)].equals(nameWinner)) {
-                    diagonalD++;
-                    if (diagonalD == lengthWinner) {
-                        winnerGamer = true;
-                        break;
-                    }
-                } else {
-                    diagonalD = 0;
-                }
-            }
-            d++;
-        }
         return winnerGamer;
-    }
+        }
 
-}
+    }
