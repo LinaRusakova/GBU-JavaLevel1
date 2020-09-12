159cd03f150a8a761f479d78f45709649bf9fe97
Lina Rusakova <xlinaris@gmail.com>
2020-09-12 22:56:21 +0300

    My homework for lesson 8, commit 1 (My GUI application based on java swing generate password for you)

---
+++ Homework/src/lesson08/MainClassGUI.java
@@ -0,0 +1,15 @@
+package lesson08;
+
+import javax.swing.*;
+
+public class MainClassGUI {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new MyFirstGUIApplication();
+            }
+        });
+    }
+
+}
